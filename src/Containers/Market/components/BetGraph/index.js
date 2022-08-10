94369462e5d1888e019dd3ceacbb6b1541e38135
duy <duydq2k1@gmail.com>
2022-08-10 08:56:00 +0700

    push

--- src/Containers/Market/components/BetGraph/index.js
+++ src/Containers/Market/components/BetGraph/index.js
@@ -1,18 +1,70 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Dimensions } from 'react-native'
 import React from 'react'
 import Graph from '@/Components/Graph'
 import Titles from '@/Components/Titles'
 import Texts from '@/Components/Texts'
 import { useTheme } from '@/Hooks'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 const BetGraph = () => {
-  const {Colors} = useTheme()
+  const { Colors } = useTheme()
   return (
     <Graph>
       <View>
-        <Titles style={{fontSize: 14}}>Market Cap</Titles>
+        <Titles style={{ fontSize: 14 }}>Market Cap</Titles>
         <Texts color={Colors.textLoss}>($3,226,646,352.00)</Texts>
       </View>
+
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: [
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100,
+                Math.random() * 100
+              ]
+            }
+          ]
+        }}
+        width={Dimensions.get("window").width} // from react-native
+        height={220}
+        yAxisLabel="$"
+        yAxisSuffix="k"
+        yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          backgroundColor: "#e26a00",
+          backgroundGradientFrom: "#fb8c00",
+          backgroundGradientTo: "#ffa726",
+          decimalPlaces: 2, // optional, defaults to 2dp
+          color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          labelColor: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+          style: {
+            borderRadius: 16
+          },
+          propsForDots: {
+            r: "6",
+            strokeWidth: "2",
+            stroke: "#ffa726"
+          }
+        }}
+        bezier
+        style={{
+          marginVertical: 8,
+          borderRadius: 16
+        }}
+      />
     </Graph>
   )
 }
--- src/Containers/Market/components/MarketCapGraph/index.js
+++ src/Containers/Market/components/MarketCapGraph/index.js
@@ -1,18 +1,62 @@
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Dimensions } from 'react-native'
 import React from 'react'
 import Graph from '@/Components/Graph'
 import Titles from '@/Components/Titles'
 import Texts from '@/Components/Texts'
 import { useTheme } from '@/Hooks'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  ProgressChart,
+  ContributionGraph,
+  StackedBarChart
+} from "react-native-chart-kit";
 
 const MarketCapGraph = () => {
-  const {Colors} = useTheme()
+  const { Colors } = useTheme()
+  
   return (
     <Graph>
       <View>
-        <Titles style={{fontSize: 14}}>Market Cap</Titles>
+        <Titles style={{ fontSize: 14 }}>Market Cap</Titles>
         <Texts color={Colors.textLoss}>($3,226,646,352.00)</Texts>
       </View>
+
+      <LineChart
+        data={{
+          labels: ["January", "February", "March", "April", "May", "June"],
+          datasets: [
+            {
+              data: [
+                0, 5, 8
+              ]
+            }
+          ]
+        }}
+        width={Dimensions.get("window").width-30} // from react-native
+        height={270}
+        // yAxisInterval={1} // optional, defaults to 1
+        chartConfig={{
+          // decimalPlaces: 1, // optional, defaults to 2dp
+          color: (opacity = 0) => `#333`,
+          labelColor: (opacity = 0) => `#333`,
+          style: {
+            borderRadius: 15
+          },
+          propsForDots: {
+            r: "0",
+            strokeWidth: "1",
+            stroke: "#333"
+          }
+        }}
+        // bezier
+        style={{
+          // marginVertical: 8,
+          borderRadius: 16,
+          backgroundColor: '#fff'
+        }}
+      />
     </Graph>
   )
 }
